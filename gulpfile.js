d4d3a76fad6ac7b6a05e54b041e09624e0367abc
Alexandr Timofeev <taratatuy@yandex.ru>
2021-06-02 11:24:55 +0000

    Move nav component to mixin, fix search button and input padding.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-const { series, parallel, src, dest } = require("gulp");
+const { series, parallel, src, dest } = require('gulp');
 
-const gulp = require("gulp"),
-  svgmin = require("gulp-svgmin"),
-  svgstore = require("gulp-svgstore"),
-  inject = require("gulp-inject"),
-  less = require("gulp-less"),
-  autoprefixer = require("gulp-autoprefixer"),
-  browserSync = require("browser-sync").create(),
-  rename = require("gulp-rename");
+const gulp = require('gulp'),
+  svgmin = require('gulp-svgmin'),
+  svgstore = require('gulp-svgstore'),
+  inject = require('gulp-inject'),
+  less = require('gulp-less'),
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync').create(),
+  rename = require('gulp-rename');
 
-gulp.task("svgstore", function () {
+gulp.task('svgstore', function () {
   const svgs = gulp
-    .src("./src/assets/icons/**/*.svg")
+    .src('./src/assets/icons/**/*.svg')
     .pipe(
       svgmin(function () {
         return {
@@ -26,7 +26,7 @@ gulp.task("svgstore", function () {
         };
       })
     )
-    .pipe(rename({ prefix: "icon-" }))
+    .pipe(rename({ prefix: 'icon-' }))
     .pipe(svgstore({ inlineSvg: true }));
 
   function fileContents(filePath, file) {
@@ -34,47 +34,48 @@ gulp.task("svgstore", function () {
   }
 
   return gulp
-    .src("./src/index.html")
+    .src('./src/index.html')
     .pipe(inject(svgs, { transform: fileContents }))
-    .pipe(gulp.dest("./src"));
+    .pipe(gulp.dest('./src'));
 });
 
-gulp.task("less", function () {
-  return src("./src/assets/styles/main.less")
+gulp.task('less', function () {
+  return src('./src/assets/styles/main.less')
     .pipe(less())
     .pipe(
       autoprefixer({
         cascade: false,
       })
     )
-    .pipe(dest("./dist"));
+    .pipe(dest('./dist'));
 });
 
-gulp.task("html", function () {
-  return gulp.src("./src/index.html").pipe(gulp.dest("./dist"));
+gulp.task('html', function () {
+  return gulp.src('./src/index.html').pipe(gulp.dest('./dist'));
 });
 
-gulp.task("fonts", function () {
-  return gulp.src("./src/assets/fonts/**/*").pipe(gulp.dest("./dist/fonts"));
+gulp.task('fonts', function () {
+  return gulp.src('./src/assets/fonts/**/*').pipe(gulp.dest('./dist/fonts'));
 });
 
-gulp.task("serve", function () {
+gulp.task('serve', function () {
   browserSync.init({
     server: {
-      baseDir: "dist",
+      baseDir: 'dist',
     },
+    port: 10259,
   });
 
-  gulp.watch("./src/assets/styles/**/*.less").on("change", series("less"));
-  gulp.watch("./src/index.html").on("change", series("html"));
+  gulp.watch('./src/assets/styles/**/*.less').on('change', series('less'));
+  gulp.watch('./src/index.html').on('change', series('html'));
 
-  gulp.watch("./dist/style.css").on("change", browserSync.reload);
-  gulp.watch("./dist/index.html").on("change", browserSync.reload);
+  gulp.watch('./dist/style.css').on('change', browserSync.reload);
+  gulp.watch('./dist/index.html').on('change', browserSync.reload);
 });
 
-gulp.task("build", series("svgstore", "fonts", "less", "html"));
+gulp.task('build', series('svgstore', 'fonts', 'less', 'html'));
 
 gulp.task(
-  "default",
-  series("svgstore", parallel("html", "less", "fonts"), "serve")
+  'default',
+  series('svgstore', parallel('html', 'less', 'fonts'), 'serve')
 );
