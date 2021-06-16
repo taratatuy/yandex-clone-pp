a6a4f8e953f752d45c70fee742a8834791ac9227
Alexandr Timofeev <taratatuy@yandex.ru>
2021-06-16 21:50:30 +0000

    Add fake zen card.

--- src/assets/scripts/API.js
+++ src/assets/scripts/API.js
@@ -6,7 +6,19 @@ class API {
   getFakeData(latency = 200) {
     return new Promise((resolve) => {
       setTimeout(() => {
-        resolve('Some Fake Data!');
+        const fakeResponce = {
+          avatarLink:
+            'https://avatars.mds.yandex.net/get-zen-logos/212539/pub_59354892e3cda85cf4157022_5b339863b5782000a9bb3bcc/36x36_2x',
+          cardName: 'Some Fake Name',
+          imgLink:
+            'https://avatars.mds.yandex.net/get-zen_doc/4355093/pub_60a58ab9ece43e0c8e0c162a_60a5f8902b5fbd2cdf669802/scale_1200',
+          cardTitle: 'Some Fake Title',
+          cardText:
+            'В России заработали новые правила для перевозчиков: теперь неоплативших штрафы ГИБДД иностранных водителей не выпустят из России до погашения долгов',
+          cardTimepast: '9 дней назад',
+        };
+
+        resolve(fakeResponce);
       }, latency);
     });
   }
--- src/assets/scripts/Zen.js
+++ src/assets/scripts/Zen.js
@@ -11,7 +11,7 @@ class Zen {
 
   addElement(content, size = 5) {
     const div = document.createElement('div');
-    div.className = 'dzen__card _span_' + size;
+    div.className = `dzen__card _span_${size} zen-card`;
     this._appendContent(div, content);
 
     this.zenAreaElem.appendChild(div);
@@ -21,6 +21,23 @@ class Zen {
     // TODO: refactor the function to append concrete structur of the card content
     // as header, image, text and author. Mb there are some parser so content should be
     // responce from the server.
-    div.innerHTML = content;
+
+    // For example:
+
+    const template = `
+      <div class="zen-card__header">
+          <img src=${content.avatarLink} class="zen-card__avatar"></img>
+          <div class="zen-card__name">${content.cardName}</div>
+      </div>
+      <a href="#" class="zen-card__main">
+          <img src=${content.imgLink} class="zen-card__img"></img>
+          <div class="zen-card__title">${content.cardTitle}</div>
+          <div class="zen-card__text">${content.cardText}</div>
+      </a>
+      <div class="zen-card__timepast">${content.cardTimepast}</div>
+      <div class="zen-card__footer"></div>
+    `;
+
+    div.insertAdjacentHTML('afterBegin', template);
   }
 }
